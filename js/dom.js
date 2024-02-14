a752754554e6dcf007092f112ec1ca026b60d61d
A1-mamun <1154almamun@gmail.com>
2024-02-14 07:45:01 +0600

    reuseable function used and all shape calculate functiom done

--- js/dom.js
+++ js/dom.js
@@ -1,69 +1,102 @@
+// Shared function fro get input by id
+function getInputById(inputFeildId) {
+  const stringValue = document.getElementById(inputFeildId).value;
+  const intValue = parseFloat(stringValue);
+  return intValue;
+}
+// Shared function for display by id
+function displayById(textFieldId, area) {
+  document.getElementById(textFieldId).innerText = area;
+}
 // triangle area function
 function calculateTriangle() {
   // get the value of base
-  const baseInput = document.getElementById("triangle-base").value;
-  const base = parseFloat(baseInput);
+  const base = getInputById("triangle-base");
 
   // get the value of height
-  const heightInput = document.getElementById("triangle-height").value;
-  const height = parseFloat(heightInput);
+
+  const height = getInputById("triangle-height");
 
   // calculate area
   let area = 0.5 * base * height;
   area = area.toFixed(3);
 
   // display area
-  document.getElementById("triangle-area").innerText = area;
+  displayById("triangle-area", area);
 }
 
 // rectangle area function
 function calculateRectangle() {
   // get the value of base
-  const widthInput = document.getElementById("rectangle-width").value;
-  const width = parseFloat(widthInput);
+  const width = getInputById("rectangle-width");
 
   // get the value of height
-  const lengthInput = document.getElementById("rectangle-length").value;
-  const length = parseFloat(lengthInput);
+  const length = getInputById("rectangle-length");
 
   // calculate area
   let area = width * length;
   area = area.toFixed(3);
   // display area
-  document.getElementById("rectangle-area").innerText = area;
+  displayById("rectangle-area", area);
 }
 
 // parallelogram are function
 function calculateParallelogram() {
   // get the value of base
-  const baseInput = document.getElementById("parallelogram-base").value;
-  const base = parseFloat(baseInput);
+  const base = getInputById("parallelogram-base");
 
   // get the value of height
-  const heightInput = document.getElementById("parallelogram-height").value;
-  const height = parseFloat(heightInput);
+  const height = getInputById("parallelogram-height");
 
   // calculate area
   let area = base * height;
   area = area.toFixed(3);
 
   // display area
-  document.getElementById("parallelogram-area").innerText = area;
+  displayById("parallelogram-area", area);
 }
 
 // rhombus are function
 function calculateRhombus() {
-  // get the valu of diameter 1
-  const d1Input = document.getElementById("rhombus-base").value;
-  const d1 = parseFloat(d1Input);
+  // get the value of diameter 1
+  const d1 = getInputById("rhombus-d1");
 
   // get the value of diameter 2
-  const d2Input = document.getElementById("rhombus-height").value;
-  const d2 = parseFloat(d2Input);
+  const d2 = getInputById("rhombus-d2");
 
   // calculate area
   let area = 0.5 * d1 * d2;
   area = area.toFixed(3);
   // display area
-  document.getElementById("rhombus-area").innerText = area;
+  displayById("rhombus-area", area);
+}
+
+// Pentagon are function
+function calculatePentagon() {
+  // get the value of perimeter
+  const perimeter = getInputById("pentagon-perimeter");
+
+  // get the value of apothem
+  const apothem = getInputById("pentagon-apothem");
+
+  // calculate area
+  let area = 0.5 * perimeter * apothem;
+  area = area.toFixed(3);
+  // display area
+  displayById("pentagon-area", area);
+}
+
+// Ellipse are function
+function calculateEllipse() {
+  // get the value of perimeter
+  const a = getInputById("ellipse-a");
+
+  // get the value of apothem
+  const b = getInputById("ellipse-b");
+
+  // calculate area
+  let area = 3.14159 * a * b;
+  area = area.toFixed(3);
+  // display area
+  displayById("ellipse-area", area);
 }
